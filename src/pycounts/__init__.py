10a70ccda197d13e3de6f6665180e50a15b4d04c
Li, Jiayi <76764434+jiayili7@users.noreply.github.com>
2025-01-09 10:20:34 -0800

    build: add pytest and pytest-cov as dev dependencies

--- src/pycounts/__init__.py
+++ src/pycounts/__init__.py
@@ -1,3 +1,17 @@
 # read version from installed package
 from importlib.metadata import version
 __version__ = version("pycounts")
+
+from pycounts.pycounts import (
+    count_words,
+    load_text,
+    clean_text
+)
+from pycounts.plotting import plot_words
+
+__all__ = [
+    "count_words",
+    "load_text",
+    "clean_text",
+    "plot_words"
+]
